f7f89c7a4f3ff0d7b03ce401c3c582bba026707f
Rebecca <rebecca.bekele@masimo.com>
2025-04-23 10:55:34 -0500

    add a paginated GET all product endpoint and post product

--- src/main/java/com/mankind/matrix_product_service/service/ProductService.java
+++ src/main/java/com/mankind/matrix_product_service/service/ProductService.java
@@ -6,8 +6,11 @@ import com.mankind.matrix_product_service.mapper.ProductMapper;
 import com.mankind.matrix_product_service.model.Product;
 import com.mankind.matrix_product_service.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -26,5 +29,17 @@ public class ProductService {
     public ProductDTO getProductById(Long id) {
         Optional<Product> product = productRepository.findById(id);
         return product.map(productMapper::toDto).orElseThrow(() -> new ProductNotFoundException(id));
+    }
+
+    public Page<ProductDTO> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable)
+                .map(productMapper::toDto);
+    }
+
+    public ProductDTO createProduct(ProductDTO dto) {
+        Product product = productMapper.toEntity(dto);
+        product.setCreateTime(LocalDateTime.now());
+        product.setUpdateTime(LocalDateTime.now());
+        return productMapper.toDto(productRepository.save(product));
     }
 }
