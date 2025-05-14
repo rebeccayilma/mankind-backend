6a7e15a9a75b1c9c50f7e6f70db3031dd1330a18
teja <ravitejaramineni520@gmail.com>
2025-05-14 13:05:27 -0500

    Adding wishlist service to the backend

---
+++ wishlist-service/src/main/java/com/example/demo/MatrixWishlishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixWishlishServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixWishlishServiceApplication.class, args);
+	}
+
+}
---
+++ wishlist-service/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateWishlistItemException.class)
+    public ResponseEntity<String> handleDuplicateItem(DuplicateWishlistItemException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleOtherErrors(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
