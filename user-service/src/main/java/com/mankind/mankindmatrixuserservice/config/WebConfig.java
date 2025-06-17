64e47d01a8c107462073f9c2331d55c2c6a70dfc
ademiltonnunes <ademilton.marcelo1@gmail.com>
2025-06-17 15:45:07 -0700

    Add maxAge to CORS configuration for improved performance

--- user-service/src/main/java/com/mankind/mankindmatrixuserservice/config/WebConfig.java
+++ user-service/src/main/java/com/mankind/mankindmatrixuserservice/config/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*");
+                        .allowedMethods("*")
+                        .maxAge(3600);
             }
         };
     }
