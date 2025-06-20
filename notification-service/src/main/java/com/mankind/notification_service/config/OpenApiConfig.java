6756574df127d3e50ec82233e10be725729bb5f0
Mallikharjuna-Surabathina <msurabathina@gmail.com>
2025-06-20 08:50:56 -0500

    Added Docker and Swagger setup for notification-service
    Includes:
    - Dockerfile with multi-stage build
    - docker-compose.yml integration with .env support
    - SMTP & Twilio env variable configuration
    - Swagger UI for REST API documentation"

---
+++ notification-service/src/main/java/com/mankind/notification_service/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.mankind.notification_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Notification API")
+                        .description("API for sending notifications to users")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Mankind Matrix AI Team")
+                                .email("support@mankind-matrix.com")
+                                .url("https://mankind-matrix.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
