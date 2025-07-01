7a05f01e8edcb06223a009fa1733e47c14dbbab6
Rebecca <rebeccayilma@gmail.com>
2025-07-01 10:42:26 -0400

    Simplify security configuration by removing unused beans and enabling OAuth2 JWT validation.

--- mankind-gateway-service/src/main/java/com/mankind/mankindgatewayservice/config/SecurityConfig.java
+++ mankind-gateway-service/src/main/java/com/mankind/mankindgatewayservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.mankind.mankindgatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,17 +12,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-//                        .pathMatchers("/api/v1/auth/**").permitAll()
-                        .anyExchange().permitAll()
-                );
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(Customizer.withDefaults())
-//                );
-
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
