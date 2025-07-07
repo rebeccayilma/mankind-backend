f8bcbfc064336569768068e4d8fb9019bb580d80
Marcelo Nunes <ademilton.marcelo1@gmail.com>
2025-07-07 13:56:41 -0700

    Delete user-service/scripts/sample_user_insert.sql

--- user-service/scripts/sample_user_insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, role, first_name, last_name, email, active, create_time, update_time)
-VALUES
-    ('admin', 'ADMIN', 'Admin', 'User', 'admin@examplle.com', true, NOW(), NOW()),
-    ('jj', 'USER', 'John', 'Doe', 'john.doe@jj.com', true, NOW(), NOW());
